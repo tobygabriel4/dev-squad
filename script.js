79edcf1f0b2fc01f67695a250ee626ac88ed2294
tobygabriel4 <tobygabriel4@gmail.com>
2024-10-17 14:43:53 +0100

    add my files

---
+++ script.js
@@ -0,0 +1,26 @@
+let result=document.getElementById("inputext");
+
+let calculate=(number)=>{
+    result.value+=number;
+
+}
+
+let Result=()=>{
+    try{
+        result.value=eval(result.value)
+        
+    } 
+    catch(err){
+        alert("enter the right input");
+        
+    }
+
+}
+
+function clr(){
+    result.value= " ";
+}
+
+function del(){
+    result.value=result.value.slice(0,-1);
+}
